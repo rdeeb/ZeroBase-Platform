ebad5c73a3a4ee4386175c07629e54851e0d0737
Ramy Deeb <blackangelr@gmail.com>
2013-09-23 09:56:08 -0500

    Adding the post type interface

---
+++ post-types/zerboase_post_type_interface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * zerobase_post_type_interface Interface for designing
+ *
+ * @author Ramy Deeb <me@ramydeeb.com>
+ * @package Zerobase
+ * @license Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. http://creativecommons.org/licenses/by-nc-nd/3.0/.
+ **/
+interface zerobase_post_type_interface
+{
+    public function configure();
+    public function getName();
+    public function getDescription();
+    public function getOptions();
+} // END interface zerobase_post_type_interface
