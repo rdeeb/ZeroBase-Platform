c1a366b0279c57fee2c68aa39ab4a46ef86f6193
Ramy Deeb <me@ramydeeb.com>
2015-07-31 13:21:08 -0500

    Creating test cache dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,8 @@
 
 require_once 'autoloader.php';
 
-define( 'ZEROBASE_CACHE_DIR',  __DIR__ . '/../cache' );
+define( 'ZEROBASE_CACHE_DIR',  __DIR__ . '/../cache/test' );
+
+if ( !is_dir( ZEROBASE_CACHE_DIR ) ) {
+    mkdir( ZEROBASE_CACHE_DIR, 755 );
+}
